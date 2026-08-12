0524d28266cab2748f98b2ffabfd70a8475e93cb
ariannaa7 <144225520+ariannaa7@users.noreply.github.com>
2026-08-12 14:39:07 +0200

    initial testing of createPregnancyCohort()

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,15 +1,27 @@
 library(TestGenerator)
 
+# CDM
 TestGenerator::readPatients(
-  filePath = "testthat/testCases/Test_data_sansComments.xlsx",
+  filePath = testthat::test_path("testCases", "Test_data_sansComments.xlsx"),
   testName = "PET",
-  outputPath = "testthat/testCases/",
+  outputPath = testthat::test_path("testCases"),
   cdmVersion = "5.4",
   extraTable = TRUE
 )
 
 cdm <- TestGenerator::patientsCDM(
-  pathJson = "testthat/testCases/",
+  pathJson = testthat::test_path("testCases"),
   testName = "PET",
   cdmVersion = "5.4"
 )
+
+# Records included in pregnancy_cohort when createPregnancyCohort() is run with default parameters
+includedWithDefaults <- tibble( # excluded will make it more difficult with twin preg 6
+  subject_id = c(11, 9, 9, 14, 17, 18, 100, 102),
+  pregnancy_id = c(6, 4, 5, 8, 10, 11, 100, 71),
+)
+
+# Re-used paths
+outputDir <- testthat::test_path("testthat_testOutput")
+pregDupFile <- file.path(outputDir, "pregnancy_duplicate_map.csv")
+
